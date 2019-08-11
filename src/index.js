8c0311c6056b55c6623c3c247d1aecd1ef154802
Ak-shay <mittalakshay2000@gmail.com>
2019-08-11 17:50:26 +0530

    hello world

--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
+const express = require('express');
+
 var mongoClient = require("mongodb").MongoClient;
 mongoClient.connect("mongodb://team450:QBni1KVLaoIor4RfSV3ouCIxquWz37rTA6aQM4Jre0r8gBUukJw9mzulNR09XFJ6cK0zjrohiPkiZmGYUY0TAw%3D%3D@team450.documents.azure.com:10255/?ssl=true", { useNewUrlParser: true }, function (err, client) {
   client.close();
 });
 
-// var app = express();
+var app = express();
 
-// app.listen(process.env.port | 8080);
+app.get('/', function(req, res){
+  res.send("hello World");
+})
+
+app.listen(process.env.port | 8080);
