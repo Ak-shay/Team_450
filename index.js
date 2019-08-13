fbfe545ec69c983c3b32b86eee6c082debe95a74
ighosh98 <indraneel.ghosh1998@gmail.com>
2019-08-13 19:18:23 +0530

    Migrations added

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const voter = require('./controllers/voter');
 const vote = require('./controllers/vote');
 const candidate = require('./controllers/candidate');
 const admin = require('./controllers/admin');
-
+const Web3 = require('web3');
 const app = express();
 
 // connecting to db
@@ -29,3 +29,39 @@ app.get('/', function(req, res){
 });
 
 app.listen(process.env.PORT || 8080);
+web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
+var account;
+web3.eth.getAccounts().then((f) => {
+ account = f[0];
+});
+abi = JSON.parse(fs.readFileSync('./Election_sol_Election.abi').toString())
+address = "0x71789831d83d4C8325b324eA9B5fFB27525480b5";
+// update this contract address with your contract address
+contract = new web3.eth.Contract(abi,address);
+candidates = {"Rama": "candidate-1", "Nick": "candidate-2", "Jose": "candidate-3"}
+
+function voteForCandidate(candidate) {
+ candidateName = $("#candidate").val();
+ console.log(candidateName);
+
+ contract.methods.voteForCandidate(web3.utils.asciiToHex(candidateName)).send({from: account}).then((f) => {
+  let div_id = candidates[candidateName];
+  contract.methods.totalVotesFor(web3.utils.asciiToHex(candidateName)).call().then((f) => {
+   $("#" + div_id).html(f);
+  })
+ })
+}
+
+$(document).ready(function() {
+ candidateNames = Object.keys(candidates);
+
+ for(var i=0; i<candidateNames.length; i++) {
+ let name = candidateNames[i];
+  
+ contract.methods.totalVotesFor(web3.utils.asciiToHex(name)).call().then((f) => {
+  $("#" + candidates[name]).html(f);
+ })
+ }
+});
+
+
