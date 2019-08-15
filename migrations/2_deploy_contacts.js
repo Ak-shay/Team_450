60bd6606783b8ddb29cdb8d831120bd6009b8e9b
Akshay Mittal <mittalakshay2000@gmail.com>
2019-08-15 15:39:27 +0530

    contract deployed

--- migrations/2_deploy_contacts.js
+++ migrations/2_deploy_contacts.js
@@ -1,5 +1,5 @@
 const Election = artifacts.require('Election');
 
 module.exports = function(deployer) {
-  deployer.deploy(Election, ['john', 'nick'].map(name => web3.utils.asciiToHex(name))); // candidates name
+  deployer.deploy(Election, ['john', 'nick'].map(name => web3.utils.asciiToHex(name)), ['bjp', 'cong'].map(name => web3.utils.asciiToHex(name))); // candidates name
 };
