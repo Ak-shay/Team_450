b36f87dc2bc4341a0812f0af48b6473dd07d14c4
Akshay Mittal <mittalakshay2000@gmail.com>
2019-08-15 15:11:54 +0530

    truffle setup

--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -1,5 +1,7 @@
 const Migrations = artifacts.require("Migrations");
+const Election = artifacts.require('Election');
 
 module.exports = function(deployer) {
+  deployer.deploy(Election, 'john', 'nick');
   deployer.deploy(Migrations);
 };
