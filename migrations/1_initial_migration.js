a44651b4ea9c2e791147227e8973bd54d4cac967
Akshay Mittal <mittalakshay2000@gmail.com>
2019-08-15 15:21:05 +0530

    contract need to be dploy

--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -1,7 +1,5 @@
 const Migrations = artifacts.require("Migrations");
-const Election = artifacts.require('Election');
 
 module.exports = function(deployer) {
-  deployer.deploy(Election, 'john', 'nick');
   deployer.deploy(Migrations);
 };
