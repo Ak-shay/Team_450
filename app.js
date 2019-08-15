07e768b6792b5f522c12280a3097a56381b7cc2e
Akshay Mittal <mittalakshay2000@gmail.com>
2019-08-15 22:10:00 +0530

    app folder is removed

--- app.js
+++ app.js
@@ -2,10 +2,10 @@ const express = require('express');
 
 const app = express();
 
-app.use(express.static('../public'));
+app.use(express.static('./public'));
 
 app.get('/', function(req, res){
     res.sendFile('index');
 });
 
-app.listen(8595);
+app.listen(8545);
