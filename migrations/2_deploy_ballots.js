0424d0e289ffb41dd3e6605dd18f83a2f139d1be
ighosh98 <indraneel.ghosh1998@gmail.com>
2019-08-18 17:16:33 +0530

    alternate version

--- migrations/2_deploy_ballots.js
+++
@@ -1,5 +0,0 @@
-const Ballot = artifacts.require('Ballot');
-
-module.exports = function(deployer) {
-  deployer.deploy(Ballot, ['john', 'nick'].map(name => web3.utils.asciiToHex(name)),['bjp', 'congress'].map(name => web3.utils.asciiToHex(name))); // candidates name
-};
